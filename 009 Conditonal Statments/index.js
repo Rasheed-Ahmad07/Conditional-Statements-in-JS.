dea92b5b3f0df86b18bb2960c73a7edc5b5c1e56
Rasheed Ahmad <118961857+Rasheed-Ahmad07@users.noreply.github.com>
2024-01-21 23:36:42 +0500

    Add files via upload

---
+++ 009 Conditonal Statments/index.js
@@ -0,0 +1,42 @@
+// console.log("sadeeq")
+// // if  else - else if -switch
+
+let
+x=280;
+
+if(x < 40)
+{
+    console.log ('the given number is less then  40');
+}
+else if( x > 100 && x <= 500){
+    console.log("given number is in between 100 and 500");
+}
+else if( x > 501 && x <= 1000){
+    console.log("given number is in between 501 and 1000");
+}
+else {
+    console.log ("the given number is greater then 1000");
+};
+
+
+// let day ="monday";
+
+// switch(day){
+//     case "peer":
+//     console.log("nan da peer oraz da");
+//     break;
+//     case "mangal":
+//     console.log("nan da Mangal oraz da");
+//     break;
+//     case "bud":
+//     console.log("nan da bud oraz da");
+//     break;
+//     case "jumarat":
+//     console.log("nan da jumarat oraz da");
+//     break;
+//     case "jumma":
+//     console.log("nan da jumma oraz da");
+//     break;
+//     default:
+//     console.log(" da ba da hafty orz yi ya da itwar");
+// }
